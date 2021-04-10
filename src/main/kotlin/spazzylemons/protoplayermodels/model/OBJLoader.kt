ee4777ea57c79cea0104d9c5722b03cddc413ce4
Matt <25674682+spazzylemons@users.noreply.github.com>
2021-04-10 08:56:12 -0400

    Clean up OBJLoader

--- src/main/kotlin/spazzylemons/protoplayermodels/model/OBJLoader.kt
+++ src/main/kotlin/spazzylemons/protoplayermodels/model/OBJLoader.kt
@@ -4,15 +4,6 @@ import net.minecraft.client.util.math.Vector3f
 import java.io.Reader
 
 object OBJLoader {
-    // yes, it's regex. no i don't care, this won't run often enough for me to feel like optimizing it.
-    private const val NUMBER = "(-?\\d+(?:\\.\\d+)?)"
-    private const val FACE = "(\\d+)/(\\d+)/(\\d+)"
-
-    private val VERTEX = Regex("v $NUMBER $NUMBER $NUMBER")
-    private val UV = Regex("vt $NUMBER $NUMBER")
-    private val NORMAL = Regex("vn $NUMBER $NUMBER $NUMBER")
-    private val QUAD = Regex("f $FACE $FACE $FACE $FACE")
-
     fun load(resource: Reader): Model {
         val vertices = mutableListOf<Vector3f>()
         val uvs = mutableListOf<UVCoordinate>()
@@ -20,53 +11,29 @@ object OBJLoader {
         val faces = mutableListOf<Face>()
 
         resource.forEachLine { line ->
-            VERTEX.find(line)?.let { vertex ->
-                vertices += Vector3f(
-                    vertex.groups[1]!!.value.toFloat(),
-                    -vertex.groups[2]!!.value.toFloat(), // flip y
-                    vertex.groups[3]!!.value.toFloat(),
-                )
-                return@forEachLine
-            }
-            UV.find(line)?.let { vertex ->
-                uvs += UVCoordinate(
-                    vertex.groups[1]!!.value.toFloat(),
-                    -vertex.groups[2]!!.value.toFloat(), // flip v
-                )
-                return@forEachLine
-            }
-            NORMAL.find(line)?.let { vertex ->
-                normals += Vector3f(
-                    vertex.groups[1]!!.value.toFloat(),
-                    -vertex.groups[2]!!.value.toFloat(), // flip y
-                    vertex.groups[3]!!.value.toFloat(),
-                )
-                return@forEachLine
-            }
-            QUAD.find(line)?.let { vertex ->
-                val v = (0..3).map { 1 + it * 3 }.map {
-                    Vertex(
-                        vertices[vertex.groups[it]!!.value.toInt() - 1],
-                        uvs[vertex.groups[it + 1]!!.value.toInt() - 1],
-                    ) to normals[vertex.groups[it + 2]!!.value.toInt() - 1]
+            when {
+                line.startsWith("v ") -> {
+                    val (x, y, z) = line.split(' ').drop(1).map(String::toFloat)
+                    vertices += Vector3f(x, -y, z) // Flip Y coordinate
                 }
-                val (v1, n1) = v[0]
-                val (v2, n2) = v[1]
-                val (v3, n3) = v[2]
-                val (v4, n4) = v[3]
-                faces += Face(
-                    v1,
-                    v2,
-                    v3,
-                    v4,
-                    n1.copy().apply {
-                        add(n2)
-                        add(n3)
-                        add(n4)
-                        normalize()
+                line.startsWith("vt ") -> {
+                    val (u, v) = line.split(' ').drop(1).map(String::toFloat)
+                    uvs += UVCoordinate(u, -v) // Flip V coordinate
+                }
+                line.startsWith("vn ") -> {
+                    val (x, y, z) = line.split(' ').drop(1).map(String::toFloat)
+                    normals += Vector3f(x, -y, z) // Flip Y coordinate
+                }
+                line.startsWith("f ") -> {
+                    val normal = Vector3f()
+                    val (a, b, c, d) = line.split(' ').drop(1).map {
+                        val (vi, uvi, ni) = it.split('/').map { i -> i.toInt() - 1 }
+                        normal.add(normals[ni])
+                        Vertex(vertices[vi], uvs[uvi])
                     }
-                )
-                return@forEachLine
+                    normal.normalize()
+                    faces += Face(a, b, c, d, normal)
+                }
             }
         }
         return Model(faces.toTypedArray())
